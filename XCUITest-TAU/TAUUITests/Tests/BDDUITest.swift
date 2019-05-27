7fa5c339452decd534016836f593291a08cb98b3
Bruno <bruno.soares@dextra-sw.com>
2019-05-27 06:17:50 -0300

    XCUITest Framework with directory Structure

---
+++ XCUITest-TAU/TAUUITests/Tests/BDDUITest.swift
@@ -0,0 +1,23 @@
+//
+//  BDDUITest.swift
+//  TAUUITests
+//
+//  Created by Bruno Soares on 5/27/19.
+//  Copyright © 2019 Shashikant Jagtap. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+class BDDUITest: TAUUITestBase {
+    func testThankYouMessageInBddStyle() {
+        givenAppIsReady()
+        whenIEnter(city: "London")
+        whenIEnrolled()
+        thenIShouldSeeThankYouMessage()
+    }
+}
+
+
+
+
---
+++ XCUITest-TAU/TAUUITests/XCUIElements/TAUElements.swift
@@ -0,0 +1,38 @@
+//
+//  TAUElements.swift
+//  TAUUITests
+//
+//  Created by Bruno Soares on 5/27/19.
+//  Copyright © 2019 Shashikant Jagtap. All rights reserved.
+//
+
+import Foundation
+import XCTest
+
+
+enum TAUScreen: String {
+    case welcomeMessage = "welcomeMessage"
+    case taulogo = "TAUlogo"
+    case enterCityLabel = "enterCity"
+    case cityTextField = "city"
+    case enrollButton = "enrollButton"
+    case thankYouMessage = "Thanks for Joining!"
+    case errorMessage = "Please Enter City"
+
+    var element: XCUIElement {
+
+        switch self {
+        case .welcomeMessage, .enterCityLabel, .thankYouMessage, .errorMessage:
+            return XCUIApplication().staticTexts[self.rawValue]
+
+        case .cityTextField:
+            return XCUIApplication().textFields[self.rawValue]
+
+        case .enrollButton:
+            return XCUIApplication().buttons[self.rawValue]
+
+        case .taulogo:
+            return XCUIApplication().images[self.rawValue]
+        }
+    }
+}
